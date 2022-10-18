a8d8d78a53785d8528a329d07a52f1bf41e2b2e5
CompOrg-RISCV <114032938+CompOrg-RISCV@users.noreply.github.com>
2022-10-18 09:02:12 -0700

    Update assembly_abstraction.s

--- src/assembly_abstraction.s
+++ src/assembly_abstraction.s
@@ -31,21 +31,19 @@ _start:
 // Plan (Declare) by making a comment for each register that you will be using and its purpose
 // These comments will be helpful to refer back to determine which register to use 
 
-// x10 set to 0x555
-// x11 set to 0x333
-// x15 contains the address to memory location 0x1f70 (0b0001 1111 0111 0000)
+// x10 set to 0x87654321
+// x11 set to 0x0fedcba9
+// x17 contains the address to memory location 0x1f70 (0b0001 1111 0111 0000)
 // x16 contains the address to the label DATA:
 
 
 
 // Initialize all of your registers.  Immediate instructions are recommended where possible
 
-    add x10,x0,x0
-    addi x10,x0, 0x55
-    lui x10,0x321
-    srli x10, x10, 12
-    lui x30, 0x86754
-    or x10, x10, x30
+    lui x10, 0x321                  // x10 = 0x0032 1000
+    srli x10, x10, 12               // x10 = 0x0000 0321
+    lui x30, 0x86754                // x30 = 0x8765 4000
+    or x10, x10, x30                // x10 = 0x8765 4321
 
     addi x11, x0, 0x33
     lui x11, 0xba9
@@ -54,14 +52,16 @@ _start:
     or x11, x11, x30
 
 // initializing x15 with 0x1f70 (Works if address is less than 20-bits)
-    lui x15, 0x1090
-    srli x15, x15, 12
+    addi x17, x0, 1                 // x17 = 0x0000 0001
+    slli x17, x17, 12               // x17 = 0x0000 1000
+    addi x18, x18, 0xf70            // x18 = 0x1111 1f70
+    or x17, x17, x18                // x17 = 0x1111 1f70 (Not the address that I want)
 
-    lui x17, 0x1f70
-    srli x17, x17, 12
+    lui x17, 0x1f70                 // x17 = 0x01f7 0000
+    srli x17, x17, 12               // x17 = 0x0000 1f70
 
 // initializing x16 with address with label DATA: 
-// required if address is greater than 20-bits or unknow
+// required if address is greater than 20-bits or unknown address
     lui x16, (DATA & 0xfff)     // obtain lower 12-bits of address which will start at bit 12
     srli x16, x16, 12           // shift the lower 12-bits to the proper bit positions
     lui x30, ((DATA & 0xfffff000) >> 12)    // Immediate the upper 20-bits into their proper location
@@ -75,18 +75,11 @@ _start:
 
 // Store into memory the result(s)
 
-    # sw x11, 0(x16)             // store 0x333 at address locaton DATA - 4
-    sw x10, 0(x15)              // store 0x555 at address location 0x1f70 + 4 (0x1f74)
-    sw x11, 0x4(x15)              // store 0x555 at address location 0x1f70 + 4 (0x1f74)
-    sw x10, 0x8(x15)              // store 0x555 at address location 0x1f70 + 4 (0x1f74)
-    sw x11, 0xc(x15)              // store 0x555 at address location 0x1f70 + 4 (0x1f74)
-    sw x10, 0x10(x15)              // store 0x555 at address location 0x1f70 + 4 (0x1f74)
-    sw x11, 0x14(x15)              // store 0x555 at address location 0x1f70 + 4 (0x1f74)
-    sw x10, 0x18(x15)              // store 0x555 at address location 0x1f70 + 4 (0x1f74)
-    sw x11, 0x1c(x15)              // store 0x555 at address location 0x1f70 + 4 (0x1f74)
+    sw x10, 0(x17)              // store 0x87654321 at address location 0x1f70 (0x1f70)
+    sw x11, 0x4(x17)            // store 0x0fedcba9 at address location 0x1f70 + 4 (0x1f74)
 
-    sw x10, 0(x17)
-    sw x11, 4(x17)
+    sw x10, 0(x16)              // store 0x87654321 at address location DATA
+    sw x11, 8(x16)              // store 0x0fedcba9 at address location DATA + 8
 
  /*
  	Add your assembly code above this line
